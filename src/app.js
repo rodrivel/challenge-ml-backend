4586915713ef7327b9172ea972407e4bfdad3b8d
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-13 12:46:53 -0300

    adding middlewares for cors and json body

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
 
 app.listen(process.env.PORT || 5000, ()=> {
     console.log(`App listening on port ${process.env.PORT || 5000 }`);
