6f429a1033c212445e037c19451e0bdced44e239
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-14 14:00:14 -0300

    add rate limit to api

--- src/app.js
+++ src/app.js
@@ -4,13 +4,23 @@ import helmet from 'helmet';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import itemsRoutes from './routes/items.route';
+import rateLimit from 'express-rate-limit';
 
+
+const limiter = rateLimit({
+    windowMs: 60000,
+    max: process.env.CALL_PER_MINUTE || 60,
+    message: {
+      error: "Too many requests"
+    }
+  });
+  
 const app = express();
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api/items', itemsRoutes);
+app.use('/api/items', limiter, itemsRoutes);
 
 
 app.listen(process.env.PORT || 5000, ()=> {
