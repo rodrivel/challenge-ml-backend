e29d90644155924533b9893f4239ea11d43efca4
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-14 13:54:51 -0300

    add helmet to improve security

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
+import helmet from 'helmet';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import itemsRoutes from './routes/items.route';
 
 const app = express();
+app.use(helmet());
+app.use(helmet.hidePoweredBy());
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/items', itemsRoutes);
