9a1d45423e7d47da4bdea02b5e949a004376f32b
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-15 00:46:14 -0300

    reordering services exports

--- src/services/items.service.js
+++ src/services/items.service.js
@@ -95,8 +95,8 @@ const getItemService = (id) => {
 }
 
 export {
-    listItemsService,
+    createResponseObject,
     getItemService,
-    ResponseService,
-    createResponseObject
+    listItemsService,
+    ResponseService
 }
