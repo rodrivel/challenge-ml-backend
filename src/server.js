ff5c460147d181f881dee70339b3f49fd62796d8
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-16 12:21:58 -0300

    fix some warnings

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from './app';
 
 // run server
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`App listening on port ${process.env.PORT || 5000}`);
+  console.log(`App listening on port ${process.env.PORT || 5000}`); // eslint-disable-line no-console
 });
